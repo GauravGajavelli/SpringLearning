d5f160047023c10216e35c413b8cadcc6e706ab4
gajavegs <gauravsg2004@gmail.com>
2025-04-17 03:53:57 -0400

    Springified project

---
+++ myfancypdfinvoices/src/main/java/com/gauravgajavelli/myfancypdfinvoices/context/MyFancyPdfInvoicesApplicationConfiguration.java
@@ -0,0 +1,27 @@
+package com.gauravgajavelli.myfancypdfinvoices.context;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.gauravgajavelli.myfancypdfinvoices.service.InvoiceService;
+import com.gauravgajavelli.myfancypdfinvoices.service.UserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyFancyPdfInvoicesApplicationConfiguration {
+
+    @Bean
+    public UserService userService() {
+        return new UserService();
+    }
+
+    @Bean
+    public InvoiceService invoiceService(UserService userService) {
+        return new InvoiceService(userService);
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+}
--- myfancypdfinvoices/src/main/java/com/gauravgajavelli/myfancypdfinvoices/web/MyFancyPdfInvoicesServlet.java
+++ myfancypdfinvoices/src/main/java/com/gauravgajavelli/myfancypdfinvoices/web/MyFancyPdfInvoicesServlet.java
@@ -1,14 +1,34 @@
 package com.gauravgajavelli.myfancypdfinvoices.web;
 
-import com.gauravgajavelli.myfancypdfinvoices.context.Application;
+import com.gauravgajavelli.myfancypdfinvoices.context.MyFancyPdfInvoicesApplicationConfiguration;
 import com.gauravgajavelli.myfancypdfinvoices.model.Invoice;
+import com.gauravgajavelli.myfancypdfinvoices.service.InvoiceService;
+import com.gauravgajavelli.myfancypdfinvoices.service.UserService;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import jakarta.servlet.ServletException;
+
 public class MyFancyPdfInvoicesServlet extends HttpServlet {
+
+    private UserService userService;
+    private ObjectMapper objectMapper;
+    private InvoiceService invoiceService;
+
+    @Override
+    public void init() throws ServletException {
+        AnnotationConfigApplicationContext ctx
+                = new AnnotationConfigApplicationContext(MyFancyPdfInvoicesApplicationConfiguration.class);
+        this.userService = ctx.getBean(UserService.class);
+        this.objectMapper = ctx.getBean(ObjectMapper.class);
+        this.invoiceService = ctx.getBean(InvoiceService.class);
+    }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -17,10 +37,10 @@ public class MyFancyPdfInvoicesServlet extends HttpServlet {
             String userId = request.getParameter("user_id");
             Integer amount = Integer.valueOf(request.getParameter("amount"));
 
-            Invoice invoice = Application.invoiceService.create(userId, amount);
+            Invoice invoice = invoiceService.create(userId, amount);
 
             response.setContentType("application/json; charset=UTF-8");
-            String json = Application.objectMapper.writeValueAsString(invoice);
+            String json = objectMapper.writeValueAsString(invoice);
             response.getWriter().print(json);
         } else {
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
@@ -40,8 +60,8 @@ public class MyFancyPdfInvoicesServlet extends HttpServlet {
                             "</html>");
         } else if (request.getRequestURI().equalsIgnoreCase("/invoices")) {
             response.setContentType("application/json; charset=UTF-8");
-            List<Invoice> invoices = Application.invoiceService.findAll();
-            response.getWriter().print(Application.objectMapper.writeValueAsString(invoices));
+            List<Invoice> invoices = invoiceService.findAll();
+            response.getWriter().print(objectMapper.writeValueAsString(invoices));
         }
     }
 }
