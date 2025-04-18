c0a4e61476352c0b8a0387724f7e0637421ff728
gajavegs <gauravsg2004@gmail.com>
2025-04-18 03:14:25 -0400

    Changed to using prototype

--- myfancypdfinvoices/src/main/java/com/gauravgajavelli/myfancypdfinvoices/context/MyFancyPdfInvoicesApplicationConfiguration.java
+++ myfancypdfinvoices/src/main/java/com/gauravgajavelli/myfancypdfinvoices/context/MyFancyPdfInvoicesApplicationConfiguration.java
@@ -4,13 +4,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import com.gauravgajavelli.myfancypdfinvoices.service.InvoiceService;
 import com.gauravgajavelli.myfancypdfinvoices.service.UserService;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class MyFancyPdfInvoicesApplicationConfiguration {
 
     @Bean
+    @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
     public UserService userService() {
         return new UserService();
     }
