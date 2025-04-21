ae7ef47fd8e83c5fad6b54a907baed312ddc182d
gajavegs <gauravsg2004@gmail.com>
2025-04-21 13:56:35 -0400

    Environment: Resources, Properties & Profiles
    Just added some environment variables and example classes for say a dev environment

--- myfancypdfinvoices/src/main/java/com/gauravgajavelli/myfancypdfinvoices/service/InvoiceService.java
+++ myfancypdfinvoices/src/main/java/com/gauravgajavelli/myfancypdfinvoices/service/InvoiceService.java
@@ -2,6 +2,9 @@ package com.gauravgajavelli.myfancypdfinvoices.service;
 
 import com.gauravgajavelli.myfancypdfinvoices.model.Invoice;
 import com.gauravgajavelli.myfancypdfinvoices.model.User;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -14,8 +17,23 @@ public class InvoiceService {
 
     private final UserService userService;
 
-    public InvoiceService(UserService userService) {
+    private final String cdnUrl;
+
+    public InvoiceService(UserService userService, @Value("${cdn.url}") String cdnUrl) {
         this.userService = userService;
+        this.cdnUrl = cdnUrl;
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Fetching PDF Template from S3...");
+        // TODO download from s3 and save locally
+    }
+
+    @PreDestroy
+    public void shutdown() {
+        System.out.println("Deleting downloaded templates...");
+        // TODO actual deletion of PDFs
     }
 
     public List<Invoice> findAll() {
@@ -30,7 +48,7 @@ public class InvoiceService {
 
 
         // TODO real pdf creation and storing it on network server
-        Invoice invoice = new Invoice(userId, amount, "http://www.africau.edu/images/default/sample.pdf");
+        Invoice invoice = new Invoice(userId, amount, cdnUrl + "/images/default/sample.pdf");
         invoices.add(invoice);
         return invoice;
     }
