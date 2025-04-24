85b36cb0f05b5879ba3c46fd855a51b94db7ba67
gajavegs <gauravsg2004@gmail.com>
2025-04-23 23:53:25 -0400

    Swapped mybank to use @ComponentScan over @Bean

--- mybank/src/main/java/com/gauravgajavelli/mybank/context/Application.java
+++ mybank/src/main/java/com/gauravgajavelli/mybank/context/Application.java
@@ -3,19 +3,18 @@ package com.gauravgajavelli.mybank.context;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.gauravgajavelli.mybank.Main;
 import com.gauravgajavelli.mybank.service.TransactionService;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@ComponentScan(basePackageClasses = Main.class)
 //@PropertySource("classpath:/application.properties")
 //@PropertySource(value = "classpath:/application-${spring.profiles.active}.properties"
 public class Application {
-    @Bean
-    TransactionService transactionService() {
-        return new TransactionService();
-    }
 
     @Bean
     public ObjectMapper objectMapper() {
