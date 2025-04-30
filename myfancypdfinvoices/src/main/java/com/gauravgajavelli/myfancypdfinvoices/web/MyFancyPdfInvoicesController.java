4b30186c178b88eb29d24244196380a3d7b758d9
gajavegs <gauravsg2004@gmail.com>
2025-04-29 22:27:13 -0400

    Adjusted Spring MVC to use JSON @RestController
    Also updated tests

---
+++ myfancypdfinvoices/src/main/java/com/gauravgajavelli/myfancypdfinvoices/web/MyFancyPdfInvoicesController.java
@@ -0,0 +1,29 @@
+package com.gauravgajavelli.myfancypdfinvoices.web;
+
+import com.gauravgajavelli.myfancypdfinvoices.dto.InvoiceDto;
+import com.gauravgajavelli.myfancypdfinvoices.model.Invoice;
+import com.gauravgajavelli.myfancypdfinvoices.service.InvoiceService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class MyFancyPdfInvoicesController {
+
+    private final InvoiceService invoiceService;
+
+    public MyFancyPdfInvoicesController(InvoiceService invoiceService) {
+        this.invoiceService = invoiceService;
+    }
+
+    @GetMapping("/invoices")
+    // @RequestMapping(value = "/invoices", method = RequestMethod.GET)
+    public List<Invoice> invoices() {
+        return invoiceService.findAll();
+    }
+
+    @PostMapping("/invoices")
+    public Invoice createInvoice(@RequestBody InvoiceDto invoiceDto) {
+        return invoiceService.create(invoiceDto.getUserId(), invoiceDto.getAmount());
+    }
+}
