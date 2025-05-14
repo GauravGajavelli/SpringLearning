3c67a710e9598f3789a0eb14f8c7eea7817294b7
gajavegs <gauravsg2004@gmail.com>
2025-05-14 14:50:05 -0400

    Add login and validation

--- myfancypdfinvoices/src/main/java/com/gauravgajavelli/myfancypdfinvoices/web/WebsiteController.java
+++ myfancypdfinvoices/src/main/java/com/gauravgajavelli/myfancypdfinvoices/web/WebsiteController.java
@@ -1,9 +1,14 @@
 package com.gauravgajavelli.myfancypdfinvoices.web;
 
 
+import com.gauravgajavelli.myfancypdfinvoices.web.forms.LoginForm;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDateTime;
@@ -16,5 +21,23 @@ public class WebsiteController {
         model.addAttribute("username", username);
         model.addAttribute("currentDate", LocalDateTime.now());
         return "index.html";
+    }
+
+    @GetMapping("/login")
+    public String login(Model model){
+        model.addAttribute("loginForm", new LoginForm());
+        return "login.html";
+    }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute @Valid LoginForm loginForm, BindingResult bindingResult, Model model ){
+        if (bindingResult.hasErrors()) {
+            return "login.html";
+        }
+        if (loginForm.getUsername().equals(loginForm.getPassword())) {
+            return "redirect:/";
+        }
+        model.addAttribute("invalidCredentials", "true");
+        return "login.html";
     }
 }
