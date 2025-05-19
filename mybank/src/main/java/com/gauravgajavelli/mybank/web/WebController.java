16af367d825b855a5f9e023d46794658dc3808b6
gajavegs <gauravsg2004@gmail.com>
2025-05-19 01:16:23 -0400

    Rough transaction form

--- mybank/src/main/java/com/gauravgajavelli/mybank/web/WebController.java
+++ mybank/src/main/java/com/gauravgajavelli/mybank/web/WebController.java
@@ -1,10 +1,14 @@
 package com.gauravgajavelli.mybank.web;
 
 import com.gauravgajavelli.mybank.service.TransactionService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class WebController
@@ -20,5 +24,14 @@ public class WebController
         System.out.println(userId);
         model.addAttribute("transactions", transactionService.getAccount(userId));
         return "account.html";
+    }
+
+    @PostMapping("/account")
+    public String login(@ModelAttribute @Valid TransactionForm transactionForm, BindingResult bindingResult, Model model ){
+        if (bindingResult.hasErrors()) {
+            return "account.html";
+        }
+        model.addAttribute("invalidCredentials", "true");
+        return "login.html";
     }
 }
