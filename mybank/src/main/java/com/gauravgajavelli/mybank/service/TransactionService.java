5fa072f03900a68515bcec12e518f2d524cc430b
gajavegs <gauravsg2004@gmail.com>
2025-05-29 23:59:04 -0400

    Add reference implementation for JDBC

--- mybank/src/main/java/com/gauravgajavelli/mybank/service/TransactionService.java
+++ mybank/src/main/java/com/gauravgajavelli/mybank/service/TransactionService.java
@@ -1,6 +1,7 @@
 package com.gauravgajavelli.mybank.service;
 
 import com.gauravgajavelli.mybank.model.Transaction;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -11,7 +12,10 @@ import java.util.stream.Collectors;
 public class TransactionService {
     private List<Transaction> transactions = new CopyOnWriteArrayList<>();
 
-    public TransactionService() {
+    private JdbcTemplate jdbcTemplate;
+
+    public TransactionService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     public List<Transaction> findAll() {
@@ -28,4 +32,47 @@ public class TransactionService {
         transactions.add(transaction);
         return transaction;
     }
+
+    /*
+    @Transactional
+    public List<Invoice> findAll() {
+        System.out.println("Is a database transaction open? = " + TransactionSynchronizationManager.isActualTransactionActive());
+        return jdbcTemplate.query("select id, user_id, pdf_url, amount from invoices", (resultSet, rowNum) -> {
+            Invoice invoice = new Invoice();
+            invoice.setId(resultSet.getObject("id").toString());
+            invoice.setPdfUrl(resultSet.getString("pdf_url"));
+            invoice.setUserId(resultSet.getString("user_id"));
+            invoice.setAmount(resultSet.getInt("amount"));
+            return invoice;
+        });
+    }
+
+    @Transactional
+    public Invoice create(String userId, Integer amount) {
+        System.out.println("Is a database transaction open? = " + TransactionSynchronizationManager.isActualTransactionActive());
+        String generatedPdfUrl = cdnUrl + "/images/default/sample.pdf";
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection
+                    .prepareStatement("insert into invoices (user_id, pdf_url, amount) values (?, ?, ?)",
+                            Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, userId);  //
+            ps.setString(2, generatedPdfUrl);
+            ps.setInt(3, amount);
+            return ps;
+        }, keyHolder);
+
+        String uuid = !keyHolder.getKeys().isEmpty() ? ((UUID) keyHolder.getKeys().values().iterator().next()).toString()
+                : null;
+
+        Invoice invoice = new Invoice();
+        invoice.setId(uuid);
+        invoice.setPdfUrl(generatedPdfUrl);
+        invoice.setAmount(amount);
+        invoice.setUserId(userId);
+        return invoice;
+    }
+    */
 }
