3132b5075dd4a14921a0a3b52d2c5235547f6b39
gajavegs <gauravsg2004@gmail.com>
2025-05-31 23:58:44 -0400

    Rough adding query to mybank

--- mybank/src/main/java/com/gauravgajavelli/mybank/service/TransactionService.java
+++ mybank/src/main/java/com/gauravgajavelli/mybank/service/TransactionService.java
@@ -3,6 +3,7 @@ package com.gauravgajavelli.mybank.service;
 import com.gauravgajavelli.mybank.model.Transaction;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
@@ -10,8 +11,6 @@ import java.util.stream.Collectors;
 
 @Component
 public class TransactionService {
-    private List<Transaction> transactions = new CopyOnWriteArrayList<>();
-
     private JdbcTemplate jdbcTemplate;
 
     public TransactionService(JdbcTemplate jdbcTemplate) {
@@ -19,7 +18,20 @@ public class TransactionService {
     }
 
     public List<Transaction> findAll() {
-        return transactions;
+        System.out.println("Is a database transaction open? = " + TransactionSynchronizationManager.isActualTransactionActive());
+        return jdbcTemplate.query("select id, user_id, pdf_url, amount from invoices", (resultSet, rowNum) -> {
+            Integer id = Integer.valueOf(resultSet.getObject("id"));
+            Integer amount = Integer.valueOf(transaction.getAmount());
+            String timestamp = String.valueOf(transaction.getTimestamp());
+            String reference = transaction.getReference();
+
+            model.addAttribute("userId", userId);
+
+            Transaction transaction = new Transaction(id, amount, timestamp, reference);
+
+            transactionService.create(id, amount, timestamp, reference);
+
+        });
     }
     public List<Transaction> getAccount(int id) {
         return transactions.stream()
